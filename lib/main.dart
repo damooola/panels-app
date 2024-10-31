b95bf3fc5699280db6ba9258ce5cafba4831f0d2
damooola <damolanelson@gmail.com>
2024-10-31 23:35:46 +0100

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:panels_app/screens/holder_screen.dart';
+import 'package:provider/provider.dart';
+import 'theme/theme_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HolderScreen(),
+    return MaterialApp(
+      home: const HolderScreen(),
       debugShowCheckedModeBanner: false,
+      theme: context.watch<ThemeProvider>().themeData,
     );
   }
 }
